3c8e974e80823421f5609514b89d62c803130c75
poplicodes26 <78361695+poplicodes26@users.noreply.github.com>
2021-03-24 20:34:35 +0530

    Prime numbers in a given range

---
+++ primeRange.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int low = scn.nextInt();
+        int high = scn.nextInt();
+        
+        for(int i=low;i<=high;i++){
+            boolean flag = true;
+            for(int j=2;j*j<=i;j++){
+                if(i%j==0){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag==true){
+                System.out.println(i);
+            }
+        }
+    }
+}
