87fa49fcb418cd70ce30a3e7aba7af6bcdab5752
poplicodes26 <manav.popli26@gmail.com>
2021-04-30 19:36:01 +0530

    rotate a 2d array by 90 deg

---
+++ Arrays/2D Arrays/rotate90.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr[][] = new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        //transpose
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        //reversing the columns
+        
+        for(int i=0;i<n;i++){
+            int l=0;
+            int r=n-1;
+            while(r>l){
+                int temp = arr[i][l];
+                arr[i][l] = arr[i][r];
+                arr[i][r] = temp;
+                l++;
+                r--;
+            }
+        }
+        
+        display(arr);
+        
+    }
+
+    public static void display(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
